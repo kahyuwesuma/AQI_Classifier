74ab448351a0e8509f98ea8fa1adc88fa77c4aed
kahyuwesuma <kahyuwesuma@gmail.com>
2023-12-13 14:53:33 +0800

    function update

--- controller/aqi_predictor_controller.py
+++ controller/aqi_predictor_controller.py
@@ -1,15 +1,16 @@
 from model.aqi_model import load_aqi_model
 from view.cluster_info_view import display_cluster_info
-from view.predict import inputs
+from view.predict import inputs, load_json
 import streamlit as st
 import json
 
 def main():   
     try:
         model = load_aqi_model('model/aqi_predictor_model.sav')
+        aqi_data = load_json('data/cluster.json')
         page = st.sidebar.selectbox("Menu", ["Prediction" ,"Cluster Information"])      
         if page == "Prediction":
-            inputs(model)
+            inputs(model, aqi_data)
         elif page == "Cluster Information":
             display_cluster_page()
     except Exception as e:
@@ -22,6 +23,3 @@ def display_cluster_page():
         display_cluster_info(cluster_info_data)
     except Exception as e:
         st.error(f"Error loading the JSON: {e}")
-
-if __name__ == "__main__":
-    main()
